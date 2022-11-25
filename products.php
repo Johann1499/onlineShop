cdce07814365a4f5af0f2eb9c2612a5d76d61a5c
Johann Christian M. Hernandez <johannhernandez1233@gmail.com>
2022-11-25 16:48:53 +0800

    Online Shop Successfully Finished

---
+++ products.php
@@ -0,0 +1,60 @@
+<?php
+    $arrProducts = array(
+        array(
+            'name'          => "Black V-Neck Tee",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dignissim vulputate tortor, sed ornare dui pharetra sed. Aliquam posuere eleifend eros nec aliquam. Aliquam ac risus a tellus mollis ultrices. Mauris tellus felis, luctus id metus nec, tristique molestie neque.",
+            'price'         => "650",
+            'shirt1'        => "t-shirt1a.png",
+            'shirt2'        => "t-shirt1b.png"
+        ),
+        array(
+            'name'          => "Under Armour Tee",
+            'description'   => "Aenean tincidunt mattis eros, non venenatis felis suscipit eu. Fusce a tempus nulla. Cras pretium purus augue, ut semper arcu pulvinar sit amet. Nam et est consectetur, mollis mi nec, pharetra nibh. Sed malesuada pellentesque ipsum, vel congue purus. Mauris eget velit vel metus ullamcorper lacinia. Sed in vestibulum erat.",
+            'price'         => "750",
+            'shirt1'        => "t-shirt2a.png",
+            'shirt2'        => "t-shirt2b.png",
+        ),
+        array(
+            'name'          => "Orange V-Neck Tee",
+            'description'   => "Donec ac neque non turpis aliquet vehicula a in eros. Vestibulum ante mauris, condimentum nec ex eget, ultrices consequat felis. Curabitur nec consequat magna. Donec euismod magna tortor, eget ultrices mauris lobortis et. Etiam imperdiet aliquet porta. Maecenas magna felis, placerat et rhoncus quis, gravida in nunc.",
+            'price'         => "650",
+            'shirt1'        => "t-shirt3a.png",
+            'shirt2'        => "t-shirt3b.png",
+        ),
+        array(
+            'name'          => "Gray Pocket Tee",
+            'description'   => "Proin rutrum volutpat ultricies. In vitae lorem vitae augue blandit lobortis. Aenean vel elit malesuada, semper diam et, tristique quam. Praesent scelerisque ipsum vitae vehicula aliquet. Morbi odio risus, consectetur in tristique eget, scelerisque nec erat. Nulla porttitor erat eget risus scelerisque, vitae gravida tortor interdum. Aliquam commodo lacinia ante sit amet commodo.",
+            'price'         => "600",
+            'shirt1'        => "t-shirt4a.png",
+            'shirt2'        => "t-shirt4b.png",
+        ),
+        array(
+            'name'          => "Black Pocket Tee",
+            'description'   => "Nullam pharetra sit amet lectus eu mattis. Cras nec blandit massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur volutpat, velit eu bibendum aliquet, justo justo sodales ligula, sit amet fermentum velit est id sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras eget scelerisque dui.",
+            'price'         => "600",
+            'shirt1'        => "t-shirt5a.png",
+            'shirt2'        => "t-shirt5b.png",
+        ),
+        array(
+            'name'          => "Yellow Pocket Tee",
+            'description'   => "Maecenas gravida viverra felis, in rutrum dolor. Nam pharetra mollis urna, eu sagittis odio luctus quis. Pellentesque vulputate arcu purus, non tincidunt ligula rutrum ac. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et nibh sed massa vulputate sagittis. Fusce sit amet odio ut dui auctor venenatis eu non leo. ",
+            'price'         => "600",
+            'shirt1'        => "t-shirt6a.png",
+            'shirt2'        => "t-shirt6b.png",
+        ),
+        array(
+            'name'          => "Orange Pocket Tee",
+            'description'   => "Morbi pharetra nisi felis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse pretium sem ut orci semper fermentum. Curabitur quis dui quis quam laoreet sollicitudin. Quisque sit amet faucibus ligula, quis pharetra lacus. Proin ut imperdiet lectus, sit amet placerat risus.",
+            'price'         => "600",
+            'shirt1'        => "t-shirt7a.png",
+            'shirt2'        => "t-shirt7b.png",
+        ), 
+        array(
+            'name'          => "Brown Pocket Tee",
+            'description'   => "Donec rutrum, ipsum ac molestie tristique, nisi leo hendrerit nibh, eu volutpat augue nibh in massa. Nulla molestie elit sit amet urna mattis, molestie pharetra tortor venenatis. In quis dolor sed urna maximus dignissim. Morbi aliquet nulla eget erat accumsan, id pretium ex varius.",
+            'price'         => "600",
+            'shirt1'        => "t-shirt8a.png",
+            'shirt2'        => "t-shirt8b.png",
+        )
+    );
+?>
